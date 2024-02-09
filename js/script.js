a46ece05554cfca4ef63c976e93571468403bb73
manueledigregorio <dig.manuele@gmail.com>
2024-02-09 10:27:30 +0100

    [add] keyup-enter

--- js/script.js
+++ js/script.js
@@ -21,12 +21,26 @@ function createInput(todo) {
   const textTodo = $(todo).children('.todotext').text();
   const input = $('.template--text').children().clone();
   todo.children('.todo__text').html(input);
-  input.val(textTodo).focus().focusout(function (event){
+
+  input.val(textTodo).focus().focusout(function (){
     console.log(event.currentTarget);
     const input = $(this);
-    if(textTodo.trim().length > 0){
 
+    if(textTodo.trim().length > 0){
+      input.parent('.todo__text').html(textTodo);
     } 
+    else{
+      input.parents('.todo__item').remove();
+    }
 
   })
+  .keyup(function (event) {
+    const input = $(this);
+    if(event.keyCodr === 27 && textTodo.trim().length > 0) {
+      input.parent('.todo__text').html(textTodo);
+    }
+    else if(event.keyCode === 27 && textTodo.trim().length === 0){
+      input.parents('.todo__item').remove();
+    }
+  });
 }
